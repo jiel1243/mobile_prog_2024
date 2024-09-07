a15127204ba35a18529db55ae4164a2fe0bdbd32
Johnloyd <2200657@ub.edu.ph>
2024-09-07 10:53:37 +0800

    Demo GIT change

--- play_demogitchange.js
+++ play_demogitchange.js
@@ -3,8 +3,22 @@ console.log("Demo GIT Change");
 // ADD, CONTACT AND PUSH
 /*
 
+1. Create a new repository on Github
+2. Go to the folder where your project is located
+3. Run git init
+4. Run git add .
+5. Run git commit -m"Initial commit"
+6. Run git Branch  -M main
+7. Run git remote add origin <url>
+8. Run git push -u origin main
+
+*/
+
+// ADD/EDIT, COMMMIT AND PUSH
+/*
+
 1. git add.
-2. git commit
+2. git commit -m"Demo GIT change"
 3. git push
 
 */
